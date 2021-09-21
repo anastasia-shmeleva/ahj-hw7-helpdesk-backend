6c79df8fa500e8588edbe45b8851337d4a0094a9
Anastasia Shmeleva <anastasiashmeleva@Anastasias-MacBook-Air-2.local>
2021-09-21 21:33:15 +0300

    read json

--- server.js
+++ server.js
@@ -1,14 +1,49 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  console.log(req.headers);
-  res.end('server response');
-});
+import * as tickets from './tickets.json';
+import Koa from 'koa';
+import koaBody from 'koa-body';
+import cors from 'koa2-cors';
+const app = new Koa();
 
-const port = 7070;
-// слушаем определённый порт
-server.listen(port, (err) => {
-    if (err) {
-      return console.log('Error occured:', error)
-    }
-    console.log(`server is listening on ${port}`)
+app.use(koaBody({
+    text: true,
+    urlencoded: true,
+    multipart: true,
+    json: true,
+}));
+
+//Cors
+app.use(
+    cors({
+      origin: '*',
+      credentials: true,
+      'Access-Control-Allow-Origin': true,
+      allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+    })
+);
+
+app.use(async (ctx) => {
+  console.log(ctx);
+  let method;
+  if (ctx.request.method === 'GET') ({ method } = ctx.request.query);
+  else if (ctx.request.method === 'POST') ({ method } = ctx.request.body);
+  
+  switch (method) {
+    case 'allTickets': ctx.response.body = tickets;
+      break;
+    // case 'ticketById': ctx.response = ticketController.getTicketFull(ctx.request.query);
+    //   break;
+    // case 'createTicket': ctx.response = ticketController.createTicket(ctx.request.body);
+    //   break;
+    // case 'changeStatus': ctx.response = ticketController.changeStatus(ctx.request.body);
+    //   break;
+    // case 'updateTicket': ctx.response = ticketController.updateTicket(ctx.request.body);
+    //   break;
+    // case 'deleteTicket': ctx.response = ticketController.deleteTicket(ctx.request.body);
+    //   break;
+    default:
+      ctx.response.status = 404;
+      ctx.response.body = `Unknown method '${method}' in request parameters`;
+  }
 });
+  
+app.listen(process.env.PORT || 7070);
